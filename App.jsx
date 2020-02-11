45d19a27638bc958d187ece8e4b9b98fd3baa764
Aaron Berdy <aaronberdy@ucla.edu>
2020-02-11 15:18:21 -0800

    moved away extra stuff to a different file

--- App.jsx
+++ App.jsx
@@ -4,79 +4,20 @@ import { StyleSheet, Text, View } from 'react-native';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
-import { ApolloProvider, useQuery } from '@apollo/react-hooks';
-import gql from 'graphql-tag';
+import { ApolloProvider } from '@apollo/react-hooks';
 
+const URL = 'https://light-the-fuse.herokuapp.com/';
+console.log(`URL: ${URL}`);
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
-  uri: 'https://light-the-fuse.herokuapp.com/',
+  uri: URL,
 });
 
 const client = new ApolloClient({
   cache,
   link,
 });
-
-const PING_QUERY = gql`
-  query pingQuery {
-      ping
-  }
-`;
-
-const ServerPong = () => {
-  const { data, loading, error } = useQuery(PING_QUERY);
-
-  if (loading) return <Text> LOADING... </Text>;
-  if (error) return <Text>ERROR</Text>;
-  if (!data) return <Text>Not found</Text>;
-
-  // eslint-disable-next-line no-console
-  console.log(data);
-
-  return (
-    <Text>
-      {`Server has resonded: ${data.ping}`}
-    </Text>
-  );
-};
-
-const PEOPLE_QUERY = gql`
-    query peopleQuery {
-        People {
-            name
-            age
-        }
-    }
-`;
-
-const People = () => {
-  const { data, loading, error } = useQuery(PEOPLE_QUERY);
-
-  if (loading) return <Text> LOADING... </Text>;
-  if (error) return <Text>ERROR</Text>;
-  if (!data) return <Text>Not found</Text>;
-
-  // eslint-disable-next-line no-console
-  console.log(data);
-
-  return (
-    <>
-      {data.People.map((person, index) => (
-        // eslint-disable-next-line react/no-array-index-key
-        <Text key={index}>
-          {person.name}
-          {' '}
-          is
-          {' '}
-          {person.age}
-          {' '}
-          years old!
-        </Text>
-      ))}
-    </>
-  );
-};
 
 const styles = StyleSheet.create({
   container: {
@@ -91,8 +32,7 @@ export default function App() {
   return (
     <ApolloProvider client={client}>
       <View style={styles.container}>
-        <ServerPong />
-        <People />
+        <Text>Hello</Text>
       </View>
     </ApolloProvider>
   );
