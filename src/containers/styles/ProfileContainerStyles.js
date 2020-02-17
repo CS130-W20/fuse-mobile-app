4a05e84145fc5dc08e7ab11f4ff396c708c577ae
Ryan Tsang <9399151+tsunamit@users.noreply.github.com>
2020-02-17 11:50:14 -0800

    Add the "create new fuse" button as an importable component (#7)
    * Remove unnecessary file
    
    * Add create fuse button to profile
    
    * Create a new fuse button component

---
+++ src/containers/styles/ProfileContainerStyles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+// import colors from '../../styles/colors';
+
+const styles = StyleSheet.create({
+  scrollView: {
+    paddingTop: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
+    flex: 1,
+  },
+  scrollViewWrapper: {
+    // backgroundColor: 'grey',
+    flex: 1,
+  },
+});
+
+export default styles;
