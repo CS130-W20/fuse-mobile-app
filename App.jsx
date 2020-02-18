92a6bbea8b09032491f040de6827f46024b577ea
ryan macbook <rvtsang@gmail.com>
2020-02-18 14:24:16 -0800

    Created a logged in component and separated it from app.jsx

--- App.jsx
+++ App.jsx
@@ -4,12 +4,9 @@ import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { NavigationContainer } from '@react-navigation/native';
-import TabNavigator from './src/navigation/AppRouter';
 
 import { URL } from './src/constants';
-
-// import { Text, View } from 'react-native';
+import LoggedIn from './src/screens/LoggedIn';
 // import styles from './src/styles/Styles';
 
 const cache = new InMemoryCache();
@@ -23,24 +20,9 @@ const client = new ApolloClient({
 });
 
 export default function App() {
-  const tabNavigator = TabNavigator();
-
   return (
     <ApolloProvider client={client}>
-      <NavigationContainer>
-        {tabNavigator}
-      </NavigationContainer>
-
-      {/* If testing your own screens, an option to test is to comment out
-      the NavigationContainer above and to use the commented out <View> and
-      uncomment the required imports up top instead */}
-
-      {/* <View style={styles.container}>
-        <Text>
-          {'\n\n\n'}
-          Replace this with the component you are testing
-        </Text>
-      </View> */}
+      <LoggedIn />
     </ApolloProvider>
   );
 }
