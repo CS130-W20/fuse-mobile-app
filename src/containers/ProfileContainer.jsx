2e48f67fdea923c0ecc515844a36f917c551d411
ryan macbook <rvtsang@gmail.com>
2020-02-25 15:08:29 -0800

    Event tile listing with mock calls on profile page

--- src/containers/ProfileContainer.jsx
+++ src/containers/ProfileContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Text,
   View,
@@ -13,22 +13,23 @@ import Spacer from '../helpers/Spacer';
 import { mockAsyncWithData } from '../helpers/mock';
 
 import styles from './styles/ProfileContainerStyles';
+import EventTile from '../components/EventTile';
 
 const bio = 'Searching for my wife.\nI am accepting snakes and champagne stealers only.\nWill you accept this rose?';
 
 export default function ProfileContainer({ navigation }) {
   const [focusedView, setFocusedView] = useState(0);
-
-  // TODO prop type should be user ID so that we can load the user here. An
-  // argument is to be made to just pass the container all the information it
-  // needs? And have the parent fetch the information? Probably not... I think
-  // this is self contained enough
   const [profileData, setProfileData] = useState({
     name: '',
     bio: '',
     score: 0,
     friendCount: 0,
     completedEventCount: 0,
+  });
+  const [profileFuses, setProfileFuses] = useState({
+    set: [],
+    lit: [],
+    completed: [],
   });
 
   const profileDataQueryParser = (queryResponse) => (
@@ -41,9 +42,20 @@ export default function ProfileContainer({ navigation }) {
     }
   );
 
+  const fuseQueryParser = (queryResponse) => (
+    queryResponse.map((fuse) => (
+      {
+        name: fuse.name,
+        owner: fuse.owner,
+        description: fuse.description,
+        state: fuse.state,
+      }
+    ))
+  );
+
   // eslint-disable-next-line no-unused-vars
   const getProfileData = async (profileId) => {
-    // make mock async call to get data associated with profile
+    // TODO replace mock call with real query
     const mockedProfileData = {
       name: 'Peter Weber',
       bio,
@@ -51,83 +63,106 @@ export default function ProfileContainer({ navigation }) {
       friendCount: 3,
       completedEventCount: 69,
     };
-    const data = await mockAsyncWithData(mockedProfileData, 3000);
+    const data = await mockAsyncWithData(mockedProfileData, 1000);
 
     setProfileData(profileDataQueryParser(data));
   };
 
-  // eslint-disable-next-line class-methods-use-this
-  const getSetFusesView = () => {
-    const mockEventTiles = [];
-    const numMockEventTiles = 10;
-    for (let i = 0; i < numMockEventTiles; i += 1) {
-      mockEventTiles.push(
-        <View style={styles.dummyView}>
-          <Text>
-            Set fuse
-            {i}
-          </Text>
-        </View>,
-      );
-    }
+  // eslint-disable-next-line no-unused-vars
+  const getProfileFuses = async (profileId) => {
+    // TODO replace mock call with real query
+    const mockedProfileFuses = [
+      {
+        name: 'Party with Peter',
+        owner: 'Peter',
+        description: 'A party!',
+        state: 1,
+      },
+      {
+        name: 'Lit Party with Peter',
+        owner: 'Peter',
+        description: 'A lit party!',
+        state: 0,
+      },
+      {
+        name: 'Plane flight',
+        owner: 'Peter',
+        description: 'Zoom zoom',
+        state: 2,
+      },
+    ];
+    const fuseData = await mockAsyncWithData(mockedProfileFuses, 1000);
+    const parsedFuseData = fuseQueryParser(fuseData);
 
-    return mockEventTiles;
-  };
+    const setFuses = [];
+    const litFuses = [];
+    const completedFuses = [];
 
-  // eslint-disable-next-line class-methods-use-this
-  const getLitFusesView = () => {
-    const mockEventTiles = [];
-    const numMockEventTiles = 10;
-    for (let i = 0; i < numMockEventTiles; i += 1) {
-      mockEventTiles.push(
-        <View style={styles.dummyView}>
-          <Text>
-            Lit fuse
-            {i}
-          </Text>
-        </View>,
-      );
-    }
+    parsedFuseData.forEach((fuse) => {
+      switch (fuse.state) {
+        case 0:
+          litFuses.push(fuse);
+          break;
+        case 1: {
+          setFuses.push(fuse);
+          break;
+        }
+        case 2: {
+          completedFuses.push(fuse);
+          break;
+        }
+        default:
+          break;
+      }
+    });
 
-    return mockEventTiles;
-  };
-
-  // eslint-disable-next-line class-methods-use-this
-  const getCompletedFusesView = () => {
-    const mockEventTiles = [];
-    const numMockEventTiles = 10;
-    for (let i = 0; i < numMockEventTiles; i += 1) {
-      mockEventTiles.push(
-        <View style={styles.dummyView}>
-          <Text>
-            Completed fuse
-            {i}
-          </Text>
-        </View>,
-      );
-    }
-
-    return mockEventTiles;
+    setProfileFuses({
+      set: setFuses,
+      lit: litFuses,
+      completed: completedFuses,
+    });
   };
 
   const showToggledView = () => {
+    let fuseListToShow;
+
     switch (focusedView) {
-      case 0: {
-        return getSetFusesView();
-      }
-      case 1: {
-        return getLitFusesView();
-      }
-      case 2: {
-        return getCompletedFusesView();
-      }
-      default: {
-        return (<View />);
-      }
+      case 0:
+        fuseListToShow = profileFuses.set;
+        break;
+      case 1:
+        fuseListToShow = profileFuses.lit;
+        break;
+      case 2:
+        fuseListToShow = profileFuses.completed;
+        break;
+      default:
     }
+
+    if (fuseListToShow.length === 0) {
+      // TODO replace with text
+      return (
+        <Text>No fuses</Text>
+      );
+    }
+
+    return fuseListToShow.map((fuse) => (
+      <EventTile
+        eventName={fuse.name}
+        eventCreator={fuse.owner}
+        description={fuse.description}
+        eventStage={fuse.state}
+        eventView={0}
+        eventRelation={0}
+        key={fuse.name}
+      />
+    ));
   };
 
-  getProfileData('');
+  useEffect(() => {
+    getProfileData('');
+    getProfileFuses('');
+  }, []);
 
   return (
     <View style={styles.wrapper}>
