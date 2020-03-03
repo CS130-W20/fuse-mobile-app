11b3c13b3ff6cff3170806c15c08babf73ce85a1
Darica Louie <darica.louie@gmail.com>
2020-03-02 17:11:35 -0800

    friend selection component added to NewFuse

--- src/screens/NewFuse.jsx
+++ src/screens/NewFuse.jsx
@@ -5,6 +5,7 @@ import {
 } from 'react-native';
 import PropTypes from 'prop-types';
 import { useMutation } from '@apollo/react-hooks';
+import Multiselect from '../components/fields/Multiselect';
 import CupertinoButtonGrey from '../components/buttons/CupertinoButtonGrey';
 import MaterialUnderlineTextbox from '../components/fields/MaterialUnderlineTextbox';
 import { CREATE_EVENT_MUTATION } from '../graphql/GeneralQueries';
@@ -27,6 +28,7 @@ const styles = StyleSheet.create({
     margin: '5%',
     backgroundColor: 'white',
     borderRadius: 10,
+    flexDirection: 'column',
   },
   loremIpsum: {
     width: 34,
@@ -53,7 +55,7 @@ const styles = StyleSheet.create({
   },
   image: {
     bottom: 20,
-    left: 275,
+    left: '85%',
     width: 88,
     height: 78,
     position: 'absolute',
@@ -67,19 +69,22 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     width: '95%',
     justifyContent: 'space-between',
-    top: 80,
-    height: 30,
+    top: 120,
+    height: 80,
     position: 'relative',
   },
   deadline: {
     flexDirection: 'row',
     width: '95%',
-    top: 130,
+    top: 80,
     height: 50,
     position: 'relative',
   },
   submit: {
-    width: '75%',
+    top: 300,
+  },
+  button: {
+    width: '80%',
     height: 50,
     bottom: 0,
     position: 'absolute',
@@ -100,15 +105,101 @@ const fuseLogo = require('../../src/assets/images/logo-fuse1.png');
 const calendarIcon = require('../../src/assets/images/calendar.png');
 
 
+const itemlist = [
+  {
+    title: 'Friends',
+    id: 0,
+    icon: {
+      uri:
+        'https://img.icons8.com/material-outlined/50/000000/user--v1.png',
+    },
+
+    children: [
+      {
+        title: 'Apple',
+        id: 10,
+      },
+      {
+        title: 'Pineapple',
+        id: 13,
+      },
+      {
+        title: 'Banana',
+        id: 14,
+      },
+      {
+        title: 'Watermelon',
+        id: 15,
+      },
+      {
+        title: 'Raspberry',
+        id: 18,
+      },
+      {
+        title: 'Orange',
+        id: 19,
+      },
+      {
+        title: 'Mandarin',
+        id: 20,
+      },
+      {
+        title: 'Papaya',
+        id: 21,
+      },
+      {
+        title: 'Lychee',
+        id: 22,
+      },
+      {
+        title: 'Cherry',
+        id: 23,
+      },
+      {
+        title: 'Peach',
+        id: 24,
+      },
+      {
+        title: 'Apricot',
+        id: 25,
+      },
+    ],
+  },
+];
+
 export default function NewFuse({ navigation }) {
   // let isSet = false;
   const [isSet, updateSet] = useState(false);
 
   const [isEditing, updateEditing] = useState(true);
 
+  const [selectingFriends, toggleFriends] = useState(false);
+
   const [eventName, setEventName] = useState('');
 
   const [createEventMutation] = useMutation(CREATE_EVENT_MUTATION);
+
+  const [friendList, friendListChange] = useState('No friends invited');
+
+  const [selectedItems, onSelectedItemsChange] = useState([]);
+
+  const getKey = (id) => (itemlist['Children'.id.title]);
+
+  const selectText = (selectedItems.length
+    ? `${selectedItems
+      .map((id, i) => {
+        let label = `${id.title}, `;
+        if (i === selectedItems.length - 2) label = `${id.title} and `;
+        if (i === selectedItems.length - 1) label = `${id.title}.`;
+        return label;
+      })
+      .join('')}`
+    : 'No friends invited');
+
+  const onConfirm = () => {
+    toggleFriends(false);
+    friendListChange(selectText);
+  };
 
   const createEvent = () => {
     createEventMutation({
@@ -123,34 +214,38 @@ export default function NewFuse({ navigation }) {
       }); */
   };
 
+  const friendSelector = () => (selectingFriends
+    ? (
+      <Multiselect
+        style={{ alignSelf: 'center' }}
+        itemlist={itemlist}
+        selectedItems={selectedItems}
+        onSelectedItemsChange={onSelectedItemsChange}
+        confirmFunc={onConfirm}
+      />
+    )
+    : (
+      <CupertinoButtonGrey
+        style={{ top: 10 }}
+        text="Add Friends"
+        onPress={() => toggleFriends(isEditing)}
+      />
+    ));
+
   const submissionButton = () => {
     const b = (isSet
       ? (
-        <View>
-          <CupertinoButtonGrey
-            text="Save"
-            style={styles.submit}
-            onPress={() => updateEditing(false)}
-          />
-          <Image
-              source={fuseLogo}
-              resizeMode="contain"
-              style={styles.image}
-            />
-        </View>
+        <CupertinoButtonGrey
+          text="Save"
+          style={styles.button}
+          onPress={() => updateEditing(false)}
+        />
       ) : (
-        <View>
-          <CupertinoButtonGrey
-            text="Submit"
-            style={styles.submit}
-            onPress={createEvent}
-          />
-          <Image
-            source={fuseLogo}
-            resizeMode="contain"
-            style={styles.image}
-          />
-        </View>
+        <CupertinoButtonGrey
+          text="Submit"
+          style={styles.button}
+          onPress={createEvent}
+        />
       )
     );
     return b;
@@ -173,11 +268,10 @@ export default function NewFuse({ navigation }) {
             placeholder="Event Description"
             editable={isEditing}
           />
-          <MaterialUnderlineTextbox
-            style={styles.nameInput}
-            placeholder="Event Invite Group"
-            editable={isEditing}
-          />
+          <View style={{ top: 60 }}>
+            <Text>{friendList}</Text>
+            {isEditing ? friendSelector() : null}
+          </View>
           <View style={styles.switch}>
             <Text style={{ color: 'rgba(129,129,129,1)' }}>Send Notifications?</Text>
             <Switch
@@ -199,7 +293,14 @@ export default function NewFuse({ navigation }) {
           </View>
           {
             isEditing ? (
-              submissionButton()
+              <View style={styles.submit}>
+                {submissionButton()}
+                <Image
+                  source={fuseLogo}
+                  resizeMode="contain"
+                  style={styles.image}
+                />
+              </View>
             ) : (
               <CupertinoButtonGrey
                 text="Edit"
