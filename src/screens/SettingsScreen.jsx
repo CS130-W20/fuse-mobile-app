9b7ec1a68af9132271b6e5358cc6ccb183344969
ryan macbook <rvtsang@gmail.com>
2020-03-04 23:48:30 -0800

    Add settings page button skeleton

--- src/screens/SettingsScreen.jsx
+++ src/screens/SettingsScreen.jsx
@@ -1,14 +1,64 @@
 import React from 'react';
 import {
+  View,
   Text,
+  TouchableOpacity,
 } from 'react-native';
+import PropTypes from 'prop-types';
+
+import styles from './styles/SettingsScreenStyles';
 
 export const settingsHeaderOptions = {
   headerShown: true,
 };
 
-export default function SettingsScreen() {
+// eslint-disable-next-line no-unused-vars
+function SettingsTile({ navigation, title, onPress }) {
   return (
-    <Text>hello</Text>
+    <TouchableOpacity
+      style={styles.tileWrapper}
+      onPress={onPress}
+    >
+      <Text style={styles.tileText}>{title}</Text>
+    </TouchableOpacity>
   );
 }
+
+// eslint-disable-next-line no-unused-vars
+export default function SettingsScreen({ navigation }) {
+  const onPressEditProfile = () => {
+  };
+
+  const onPressLogout = () => {
+  };
+
+  return (
+    <View style={styles.wrapper}>
+      <Text style={styles.sectionHeaderText}>ACCOUNT</Text>
+      <SettingsTile
+        title="Edit profile"
+        onPress={onPressEditProfile}
+      />
+      <SettingsTile
+        title="Logout"
+        onPress={onPressLogout}
+      />
+    </View>
+  );
+}
+
+SettingsTile.propTypes = {
+  title: PropTypes.string.isRequired,
+  onPress: PropTypes.func.isRequired,
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+    goBack: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+SettingsScreen.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+    goBack: PropTypes.func.isRequired,
+  }).isRequired,
+};
