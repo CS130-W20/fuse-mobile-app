d1663cd67e0126438c4ad8ff5c287186d982284a
Chiara Mooney <34109996+chiaramooney@users.noreply.github.com>
2020-03-08 23:58:25 -0700

    Connect Search Frontend with Backend (#105)
    * add touchable opacity to eventTile
    
    * fix small eventTile bug
    
    * search bar
    
    * add search entries and fix tile format
    
    * try this again?
    
    * fix event tiles in user profile, add README contents
    
    * fix color references
    
    * Delete package-lock.json
    
    * connect frontend search with backend
    
    * merge with dev
    
    Co-authored-by: Ryan Tsang <9399151+tsunamit@users.noreply.github.com>

--- src/containers/ExploreContainer.jsx
+++ src/containers/ExploreContainer.jsx
@@ -1,22 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   View,
   ScrollView,
 } from 'react-native';
 
+import { useQuery } from '@apollo/react-hooks';
 import SearchBar from '../components/SearchBar';
 import FriendTile from '../components/FriendTile';
+import {
+  FRIEND_SEARCH,
+} from '../graphql/GeneralQueries';
 
 export default function ExploreContainer() {
+  const [searchText, setSearchText] = useState('');
+  const [friendTiles, setFriendTiles] = useState(<View />);
+  const {
+    data: friendSearchQueryData,
+    loading: friendSearchQueryLoading,
+    // eslint-disable-next-line no-unused-vars
+  } = useQuery(FRIEND_SEARCH, {
+    variables: {
+      prefix: searchText,
+    },
+    skip: searchText === '',
+  });
+  useEffect(() => {
+    if (friendSearchQueryData && !friendSearchQueryLoading) {
+      // console.log(friendSearchQueryData);
+      if (searchText !== '') {
+        setFriendTiles(friendSearchQueryData.users.map((user) => (
+          <FriendTile
+            userName={user.name}
+            userId={user.id}
+          />
+        )));
+      }
+    }
+  }, [friendSearchQueryData]);
+  const friendTilesToRender = (text) => {
+    setSearchText(text);
+    if (text === '') {
+      setFriendTiles(<View />);
+    }
+  };
+
   return (
     <View>
-      <SearchBar />
+      <SearchBar onChangeText={friendTilesToRender} />
       <ScrollView>
-        <FriendTile userName="chiara.mooney" />
-        <FriendTile userName="chiara.mooney" />
-        <FriendTile userName="chiara.mooney" />
-        <FriendTile userName="chiara.mooney" />
-        <FriendTile userName="chiara.mooney" />
+        {friendTiles}
       </ScrollView>
     </View>
   );
