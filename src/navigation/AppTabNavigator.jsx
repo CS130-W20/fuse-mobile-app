21d40084d69fed481ecc2a58d6f0228b83bdd046
Christian <christian@trifigo.com>
2020-03-09 01:56:34 -0700

    FINALLY FOUND A WAY TO PRESS TAB BUTTONS

--- src/navigation/AppTabNavigator.jsx
+++ src/navigation/AppTabNavigator.jsx
@@ -130,6 +130,7 @@ const tabScreenOptions = {
         ? <MaterialCommunityIcons name="home-outline" size={30} color={focusedTabColor} />
         : <MaterialCommunityIcons name="home-outline" size={30} color={unfocusedTabColor} />
     ),
+    tabBarTestID: 'goToNewsfeed',
   },
   explore: {
     // eslint-disable-next-line react/prop-types
@@ -138,6 +139,7 @@ const tabScreenOptions = {
         ? <MaterialIcons name="search" size={30} color={focusedTabColor} />
         : <MaterialIcons name="search" size={30} color={unfocusedTabColor} />
     ),
+    tabBarTestID: 'goToExplore',
   },
   notifications: {
     // eslint-disable-next-line react/prop-types
@@ -146,6 +148,7 @@ const tabScreenOptions = {
         ? <MaterialIcons name="notifications-none" size={30} color={focusedTabColor} />
         : <MaterialIcons name="notifications-none" size={30} color={unfocusedTabColor} />
     ),
+    tabBarTestID: 'goToNotifications',
   },
   profile: {
     // eslint-disable-next-line react/prop-types
@@ -154,11 +157,12 @@ const tabScreenOptions = {
         ? <MaterialCommunityIcons name="account-outline" size={30} color={focusedTabColor} />
         : <MaterialCommunityIcons name="account-outline" size={30} color={unfocusedTabColor} />
     ),
+    tabBarTestID: 'goToProfile',
   },
 };
 
 const tabBarOptions = {
-  showLabel: false, // hides text labels for navigation bar
+  showLabel: true, // hides text labels for navigation bar
 };
 
 // Create the tab navigator using react navigation
@@ -171,7 +175,7 @@ export function AppTabNavigator() {
   return (
     <Tab.Navigator
       tabBarOptions={tabBarOptions}
-      initialRouteName={NavigationStackIds.newsFeed}
+      initialRouteName={NavigationStackIds.feed}
     >
       <Tab.Screen
         name={NavigationStackIds.feed}
